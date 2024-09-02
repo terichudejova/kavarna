aff414f05730e5dd962617dff363973d76a35e42
Teri Chudějová <tereza.chudejova@gmail.com>
2024-09-02 12:58:30 +0200

    add navbar - logo, menu links

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id='mainContainer'>
+      
+      {/* FIRST SECTION - HEADER */}
+      <div id='kdoJsme' className='header outerBox'>
+        <div className="innerBox">
+          <img id='logo' src='images/logo.jpg' href="#kdoJsme"/>
+          <ul>
+            <li><a href='#kdoJsme'>Kdo jsme</a></li>
+            <li><a>Nabídka</a></li>
+            <li><a>Kontakt</a></li>
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+
+
+
+    </div>
   )
 }
 
